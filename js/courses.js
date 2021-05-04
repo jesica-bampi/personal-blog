ac95753f81922ba69b9d15363e74617760b6ea5c
Jesica Bampi <jesica.bampi@gmail.com>
2021-05-04 14:51:06 +0100

    x

--- js/courses.js
+++ js/courses.js
@@ -13,20 +13,19 @@ request.onload = function(){
 request.send();
 
 function montarLista(jsonObj) {
-    for (i = 0; i < jsonObj.length; i++) {
+    for (i = 0; i < jsonObj.lista.length; i++) {
         var listItem = document.createElement('li');
         var myH3 = document.createElement('h3');
         var mylabel1 = document.createElement('label')
         var mylabel2 = document.createElement('label');
 
-        myH3.textContent = jsonObj['curso'];
-        mylabel1.textContent = jsonObj['ano']
-        mylabel2.textContent = jsonObj['instituicao'];
+        myH3.textContent = jsonObj.lista['curso'];
+        mylabel1.textContent = jsonObj.lista['ano']
+        mylabel2.textContent = jsonObj.lista['instituicao'];
 
         listItem.appendChild(myH3);
         listItem.appendChild(mylabel1);
         listItem.appendChild(mylabel2);
-
         listaCursos.appendChild(listItem);
         //console.log(jsonObj.lista[i])
     };
